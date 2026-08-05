c2e8dd0408bc801a6d47367a3da136683288cdbf
Luciano Paz <luciano.paz.neuro@gmail.com>
2026-08-05 16:30:29 +0200

    Add autodiff_utils

--- slsqp_jax/sqpdax/__init__.py
+++ slsqp_jax/sqpdax/__init__.py
@@ -1,9 +1,10 @@
-from . import dual, primal, registry, types
+from . import autodiff_utils, dual, primal, registry, types
 from .dual import Dual
 from .primal import InteriorPointPrimal, Primal, Slack
 from .types import InitializableModule
 
 __all__ = [
+    "autodiff_utils",
     "dual",
     "primal",
     "registry",
