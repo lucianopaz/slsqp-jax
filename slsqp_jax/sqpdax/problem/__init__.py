ff8f2163eb44f1d4a1e09d7ee5770781a6685fea
Luciano Paz <luciano.paz.neuro@gmail.com>
2026-08-06 12:05:24 +0200

    Populate problem subpackage __init__

---
+++ slsqp_jax/sqpdax/problem/__init__.py
@@ -0,0 +1,12 @@
+from . import basic, builder
+from .basic import EvaluatedProblem, Problem, ProblemProtocol
+from .builder import build_problem
+
+__all__ = [
+    "basic",
+    "builder",
+    "EvaluatedProblem",
+    "ProblemProtocol",
+    "Problem",
+    "build_problem",
+]
