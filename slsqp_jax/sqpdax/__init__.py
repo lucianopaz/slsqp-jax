27fdbc9fb8507586d5095d3fce01cf6b9e04db2e
Luciano Paz <luciano.paz.neuro@gmail.com>
2026-08-06 12:08:51 +0200

    Fixup problem __init__

--- slsqp_jax/sqpdax/__init__.py
+++ slsqp_jax/sqpdax/__init__.py
@@ -1,6 +1,7 @@
-from . import autodiff_utils, dual, primal, registry, secant, types
+from . import autodiff_utils, dual, primal, problem, registry, secant, types
 from .dual import Dual
 from .primal import InteriorPointPrimal, Primal, Slack
+from .problem import EvaluatedProblem, ProblemProtocol, Problem, build_problem
 from .secant import LBFGS, CurvatureDiagnostics, Secant
 from .types import InitializableModule
 
@@ -8,6 +9,11 @@ __all__ = [
     "autodiff_utils",
     "dual",
     "primal",
+    "problem",
+    "EvaluatedProblem",
+    "ProblemProtocol",
+    "Problem",
+    "build_problem",
     "registry",
     "secant",
     "types",
